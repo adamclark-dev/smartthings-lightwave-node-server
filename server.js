265e98f987aae00a308d0b04dc8908934dc0dffb
Chris Lambourne <33834551+clinkadink@users.noreply.github.com>
2017-11-20 13:55:49 +0000

    Updated - now allows for inline relay parameters

--- server.js
+++ server.js
@@ -17,6 +17,21 @@ router.register('/register', function(req, res) {
 	helpers.lwrfRegister(req, res);
 });
 
+router.register('/close', function(req, res) {
+	console.log('Close request received');
+	helpers.lwrfRelay(0, req, res);
+});
+
+router.register('/open', function(req, res) {
+	console.log('Open request received');
+	helpers.lwrfRelay(1, req, res);
+});
+
+router.register('/stop', function(req, res) {
+	console.log('Stop request received');
+	helpers.lwrfRelay(2, req, res);
+});
+
 var server = http.createServer(function (req, res) {
 	handler = router.route(req);
 	handler.process(req, res);
